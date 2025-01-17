3621de5a7fbcd3382d709b60b3f453e3800991ab
Rapha761 <raphael.jacob@studbocconi.it>
2025-01-17 19:58:29 +0100

    Update google_sheets_integration.py

--- google_sheets_integration.py
+++ google_sheets_integration.py
@@ -6,7 +6,7 @@ import pandas as pd
 def load_google_sheets():
     # Authenticate with Google Sheets
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("google_sheets_credentials.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name("new_key.json", scope)
     client = gspread.authorize(creds)
 
     # Open the Google Sheet
@@ -20,7 +20,7 @@ def load_google_sheets():
 def add_listing_to_google_sheets(name, rent, unit_type, residence, location_features):
     # Authenticate with Google Sheets
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("google_sheets_credentials.json", scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name("new_key.json", scope)
     client = gspread.authorize(creds)
 
     # Open the Google Sheet
