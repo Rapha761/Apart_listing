fb526312e30695a79abc938f80cd1e08c30a954a
Rapha761 <raphael.jacob@studbocconi.it>
2025-01-18 13:56:41 +0100

    Update google_sheets_integration.py

--- google_sheets_integration.py
+++ google_sheets_integration.py
@@ -13,25 +13,24 @@ def load_google_sheets():
     return pd.DataFrame(data)
 
 # Add a new listing to Google Sheets
-def add_listing_to_google_sheets(name, dates, rent, unit_type, residence, address, amenities, location_features, message, contact):
+def add_listing_to_google_sheets(name, dates, rent, unit_type, residence, address, amenities, location_features, message):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
     client = gspread.authorize(creds)
     sheet = client.open("Listing_form").sheet1
-
     new_row = [
-        pd.Timestamp.now().strftime("%d/%m/%Y"),
-        pd.Timestamp.now().strftime("%H:%M:%S"),
-        name, dates, "NA", "NA", rent, unit_type, residence, address, amenities,
-        location_features, message, contact, "Offering"
+        name, dates, "NA", "NA", rent, unit_type, residence, address, amenities, location_features, message, "NA", "Supply"
     ]
     sheet.append_row(new_row)
 
-# Update contact in Google Sheets
-def update_contact(name, phone_number):
+# Update contact details in Google Sheets
+def update_contact_in_google_sheets(row, contact):
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
-    client = gspread.authorize
+    client = gspread.authorize(creds)
+    sheet = client.open("Listing_form").sheet1
+    cell = sheet.find(row['name'])  # Assuming 'Name' is a unique identifier
+    sheet.update_cell(cell.row, cell.col + 11, contact)  # Adjust for 'Contact' column index
 
 
 
